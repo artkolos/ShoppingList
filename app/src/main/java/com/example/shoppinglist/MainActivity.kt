71a7a586c6e34a481ea8b2cb23a15f7c2e3aecd3
Clip228 <artemkolos_03@mail.ru>
2023-02-07 00:04:36 +0700

    Create MainViewModel.kt

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoppinglist.presentation.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this, Observer {
+            Log.d("MyLog", it.toString())
+        })
     }
 }
--- app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,30 +1,42 @@
 package com.example.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 
-object ShopListRepositoryImp : ShopListRepository {
+object ShopListRepositoryImpl : ShopListRepository {
 
     private val shopList = mutableListOf<ShopItem>()
 
+    private val liveData = MutableLiveData<List<ShopItem>>()
 
     private var autoIncrementId = 0
 
+    init {
+        for (i in 0 until 10){
+            val item = ShopItem("Name$i", i, true)
+            addShopListItem(item)
+        }
+    }
     override fun addShopListItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID){
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateShopList()
     }
 
     override fun deleteItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateShopList()
     }
 
     override fun editItem(shopItem: ShopItem) {
         val oldElement = getItemById(shopItem.id)
         shopList.remove(oldElement)
         addShopListItem(shopItem)
+        updateShopList()
     }
 
     override fun getItemById(id: Int): ShopItem {
@@ -32,7 +44,11 @@ object ShopListRepositoryImp : ShopListRepository {
             ?: throw java.lang.RuntimeException("Element id $id not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return liveData
+    }
+
+    fun updateShopList(){
+        liveData.value = shopList.toList()
     }
 }
